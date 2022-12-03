aa1060deda8a3814b2f5929bc414ef180f301d0f
Carlos Aguirre <craguirre952@gmail.com>
2022-12-03 07:50:53 -0500

    copy Home.vue code from old repo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,155 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>
+      <!-- Search Bar -->
+      </br>
+      <input type="text" v-model="search" placeholder="Search..">
+      </br>
+      </br>
+      
+      <!-- Add Recipe -->
+      <AddRecipe @addCocktail="addNewCocktail" />
+    </div>
+    </br>
+    <hr style="width:60%">
+
+    <!-- Recipe Component -->
+    <Recipe
+      v-for="cocktail in cocktailList"
+      @removeCocktail=deleteCocktail
+      @editRecipe=editCocktail
+      :cocktail="cocktail" 
+      :key="cocktail.id" 
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from "axios";
+  import Recipe from '@/components/Recipe.vue'
+  import AddRecipe from '@/components/AddRecipe.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    components: {
+      AddRecipe,
+      Recipe
+    },
+    data: function () {
+      return {
+        cocktails: [],
+        search: "",
+      };
+    },
+    mounted: function () {
+      this.cocktailsIndex();
+
+    },
+    computed: {
+      cocktailList() {
+        return this.cocktails.filter(cocktail => 
+          cocktail.ingredient.toLowerCase().includes(this.search.toLowerCase())
+        );
+      },
+      filterCocktails() {
+        return this.cocktails.filter(cocktail =>
+          cocktail.ingredient.toLowerCase().includes(this.search.toLowerCase())
+        );
+      }
+    },
+    methods: {
+      cocktailsIndex: function () {
+        console.log("in cocktails index");
+        axios.get("/cocktails").then((response) => {
+          // console.log(response.data);
+          this.cocktails = response.data;
+        });
+      },
+      addNewCocktail: function (cocktail) {
+        console.log("new cocktail", cocktail);
+        this.cocktails = [
+          cocktail, ...this.cocktails
+            // spread operator
+        ];
+      },
+      deleteCocktail: function (cocktailId) {
+        console.log("delete cocktail", cocktailId);
+        this.cocktails = this.cocktails.filter((cocktail) => cocktail.id !== cocktailId);
+      },
+      editCocktail: function (cocktail) {
+        console.log("edit cocktail", cocktail);
+      }
+    },
+  };
 </script>
+
+<style>
+#original {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.pre-formatted {
+  white-space: pre;
+}
+.modal {
+  width: 500px;
+  margin: 0px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 7px;
+  box-shadow: 0 2px 8px 3px;
+  transition: all 0.2s ease-in;
+  font-family: Helvetica, Arial, sans-serif;
+}
+.fadeIn-enter {
+  opacity: 0;
+}
+.fadeIn-leave-active {
+  opacity: 0;
+  transition: all 0.2s step-end;
+}
+.fadeIn-enter .modal,
+.fadeIn-leave-active.modal {
+  transform: scale(1.1);
+}
+#container {
+  text-align: center;
+}
+button {
+  background-color: #04AA6D;
+  border: none;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  border-radius: 10px;
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: whitesmoke;
+  z-index: 999;
+  transition: opacity 0.2s ease;
+}
+</style>
