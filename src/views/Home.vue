b689473198ec47fcbc53f3d6947468c3968176de
Carlos Aguirre <craguirre952@gmail.com>
2022-12-04 18:12:20 -0500

    move cocktail title to inside original ID. create App ID for search filter and Add button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -82,6 +82,13 @@
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  
+}
 #original {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
