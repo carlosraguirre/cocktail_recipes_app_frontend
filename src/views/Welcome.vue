e42be019ffa95540efb40906a5138311ec5c5895
Carlos Aguirre <craguirre952@gmail.com>
2023-12-07 20:32:03 -0500

    set up template for welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,4 +1,13 @@
 <template>
+  <div>
+    <h2>What the site is</h2>
+
+    <h2>Login / Sign Up</h2>
+    <h4>use container for sign in/up</h4>
+
+    <h2>Look here for site updates!</h2>
+  </div>
+  
 </template>
 
 <script>
