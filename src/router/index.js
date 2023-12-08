849fdeef82c1177854e910168a2570d229aae632
Carlos Aguirre <craguirre952@gmail.com>
2023-12-07 20:14:26 -0500

    add new Welcome page and make default

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Login from '../views/Login.vue';
 import Logout from "../views/Logout.vue";
 import MyRolodex from "../views/MyRolodex.vue";
+import Welcome from "../views/Welcome.vue";
 import 'bootstrap';
 
 Vue.use(VueRouter)
@@ -22,7 +23,12 @@ const routes = [
     path: "/myrolodex",
     name: "myrolodex",
     component: MyRolodex
-  },  
+  },
+  {
+    path: "/",
+    name: "welcome",
+    component: Welcome
+  },   
 ]
 
 const router = new VueRouter({
