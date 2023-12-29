00d560f015035732dac0fe9d1f5a34a9c2e0df24
Carlos Aguirre <craguirre952@gmail.com>
2023-12-29 11:39:10 -0500

    create Update Corner component and add Nov 2023 update

---
+++ src/components/UpdatesCorner.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="container px-5 pb-5">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-2">
+            <img class="img-fluid" src="assets/Welcome3.jpeg" alt="..."/>
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-2">
+            <h1 class="mb-3"><b>Updates Corner!</b></h1>
+              <p>
+                Check here for updates made to this site.
+              </p>
+              <table class="table table-borderless">
+                <tbody>
+                  <tr>
+                    <th scope="row">Dec. 2023</th>
+                    <td>
+                      <p>
+                        Individual profiles feature is live. Anyone can create an account and add recipes to their Rolodex! Check the  video in the About section to see a tutorial.
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <th scope="row">Nov. 2023</th>
+                    <td>
+                      <p>
+                        Added two features: Favorites and Tags.
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <th scope="row">Mar. 2023</th>
+                    <td>
+                      <p>
+                        Cocktail Rolodex's first go-live. Currently only one user, the site owner, can add and update recipes. But anyone can view his Rolodex.
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+
+              </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -95,46 +95,7 @@
         </div>
         
         <!-- Updates -->
-        <div class="container px-5 pb-5">
-          <div class="row gx-5 align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-2">
-                <img class="img-fluid" src="assets/Welcome3.jpeg" alt="..."/>
-              </div>
-            </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-2">
-                <h1 class="mb-3"><b>Updates Corner!</b></h1>
-                  <p>
-                    Check here for updates made to this site.
-                  </p>
-                  <table class="table table-borderless">
-                    <tbody>
-                      <tr>
-                        <th scope="row">Dec. 2023</th>
-                        <td>
-                          <p>
-                            Individual profiles feature is live. Anyone can create an account and add recipes to their Rolodex! Check the  video in the About section to see a tutorial.
-                          </p>
-                        </td>
-                      </tr>
-                    </tbody>
-                    <tbody>
-                      <tr>
-                        <th scope="row">Mar. 2023</th>
-                        <td>
-                          <p>
-                            Cocktail Rolodex's first go-live. Currently only one user, the site owner, can add and update recipes. But anyone can view his Rolodex.
-                          </p>
-                        </td>
-                      </tr>
-                    </tbody>
-
-                  </table>
-              </div>
-            </div>
-          </div>
-        </div>
+        <UpdatesCorner />
 
       </div>
     </div>
@@ -153,10 +114,12 @@
 <script>
   import axios from "axios";
   import LoginSignUp from '../components/LoginSignUp.vue';
+  import UpdatesCorner from '../components/UpdatesCorner.vue';
 
   export default {
     components: {
-      LoginSignUp
+      LoginSignUp,
+      UpdatesCorner
     },
 
     data: function () {
